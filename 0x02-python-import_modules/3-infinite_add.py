1ec84823bf2a092339923f3e420d9efb2a8edfee
fdetun <fouedads@gmail.com>
2020-04-30 19:59:50 +0100

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     addition = 0
     from sys import argv
-    for i in range(1,len(argv)):
+    for i in range(1, len(argv)):
         addition = addition + int(argv[i])
     print("{:d}".format(addition))
