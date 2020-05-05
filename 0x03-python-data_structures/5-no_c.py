b9014ab2739e6e7fd770bfb1193a536a1ac35891
fdetun <fouedads@gmail.com>
2020-05-05 15:48:51 +0100

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    ss = ""
+    for i in range(1, len(my_string)):
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            continue
+        else:
+            ss = ss + my_string[i]
+    return ss
