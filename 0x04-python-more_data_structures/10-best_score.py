3eebffc17dd3f943911d2080a9821f3c89cf236c
fdetun <fouedads@gmail.com>
2020-05-08 16:28:13 +0100

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    rslt=""
+    rslt = ""
     if a_dictionary is None or a_dictionary is {}:
         return None
     else:
         maxi = sorted(a_dictionary.values())
         key = list(a_dictionary.keys())
         for i in key:
-            if a_dictionary[i] == maxi[len(maxi) - 1] :
+            if a_dictionary[i] == maxi[len(maxi) - 1]:
                 rslt = i
     return rslt
