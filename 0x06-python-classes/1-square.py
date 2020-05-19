f3968f4cbe778d62bc2682a6c9b174eec992e143
fdetun <fouedads@gmail.com>
2020-05-19 21:37:40 +0100

    fix comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,5 +3,19 @@
 
 
 class Square:
+    """
+    This is a class named square.
+
+    Attributes:
+        self (int): self.
+        size (int): number.
+    """
+
     def __init__(self, size=None):
+        """This is a function.
+
+        Attributes:
+        self (int): self.
+        size (int): number.
+        """
         self.__size = size
