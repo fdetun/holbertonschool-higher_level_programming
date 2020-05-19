594028d0fe39d8a41dac0c030d285696f6721652
fdetun <fouedads@gmail.com>
2020-05-19 21:45:32 +0100

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
+"""class Square that defines a square"""
+
+
 class Square:
+    """
+    This is a class named square.
+
+    Attributes:
+        self (int): self.
+        size (int): number.
+    """
+
     def __init__(self, size=0):
-        if isinstance(size, int) == False:
+        """This is a function.
+
+        Attributes:
+        self (int): self.
+        size (int): number.
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
