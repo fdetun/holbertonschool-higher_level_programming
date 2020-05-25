7a7bd9c33eef90a70c71a032bb5815df70803773
fdetun <fouedads@gmail.com>
2020-05-25 10:54:13 +0100

    5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+ "5-text_indentation" module.
+"""
+
+
+def text_to_array(text):
+    """
+    text_to_array
+    Args:
+        text : string
+    Returns:
+        liss
+    """
+    delim = ".:?"
+    a = 0
+    liss = []
+
+    for f in range(0, (len(text))):
+        if text[f] in delim:
+            liss.append(text[a:f + 1])
+            a = f + 1
+    return liss
+
+
+def text_indentation(text):
+    """
+    text_indentation
+    Args:
+        text : string
+    Returns:
+        None
+    """
+    if not type(text) is str:
+        raise TypeError("text must be a string")
+    liss = text_to_array(text)
+    for i in liss:
+        print("{}".format(i.strip()))
+        print()
