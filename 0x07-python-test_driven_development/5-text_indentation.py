5e332d617beb415fd52e5167bd116202035f1ad2
fdetun <fouedads@gmail.com>
2020-05-25 11:26:11 +0100

    555

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -15,14 +15,16 @@ def text_to_array(text):
     delim = ".:?"
     a = 0
     liss = []
+    new = []
 
     for f in range(0, (len(text))):
         if text[f] in delim:
             liss.append(text[a:f + 1])
             a = f + 1
-    if text[:-1] not in delim and text[a + 1] != " ":
+    if text[:-1] not in delim :
         liss.append(text[a:len(text)])
-    return liss
+    new = filter(lambda x: x != "", liss)
+    return new
 
 
 def text_indentation(text):
