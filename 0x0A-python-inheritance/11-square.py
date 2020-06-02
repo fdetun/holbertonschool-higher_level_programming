e6d7bb4003fabb3383069a134addc445816d0094
fdetun <fouedads@gmail.com>
2020-06-02 22:44:16 +0100

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+class Square that inherits a class Rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Sqaure class"""
+    def __init__(self, size):
+        """init func of a Square class"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self._fde = size
+
+    def area(self):
+        return (self._fde ** 2)
+
+    def __str__(self):
+        return ("[Square] {:d}/{:d}".format(self._wh, self._ht))
