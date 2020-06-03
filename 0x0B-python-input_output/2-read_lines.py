2c62fb9c8c8f901a8fda34556b3368763c05664b
fdetun <fouedads@gmail.com>
2020-06-03 17:10:00 +0100

    2-read_lines.py

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -10,7 +10,7 @@ def read_lines(filename="", nb_lines=0):
     nb_lines = int
     """
     with open(filename, encoding='utf8') as txtfile:
-        if nb_lines != 0:
+        if nb_lines > 0:
             for i in range(nb_lines):
                 fde = txtfile.readline()
                 print(fde, end="")
