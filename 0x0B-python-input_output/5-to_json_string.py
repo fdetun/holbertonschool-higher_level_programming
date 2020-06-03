87facb12d33b8c6e443816693ca58d57078e469b
fdetun <fouedads@gmail.com>
2020-06-03 18:16:51 +0100

    5-to_json_string.py

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""to_json_string python script"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    to_json_string python func
+    args :
+    my_obj
+    """
+    return json.dumps(my_obj)
