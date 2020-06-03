3388f79fa660e98e65ea3ea76696206e3c7e189b
fdetun <fouedads@gmail.com>
2020-06-03 23:12:54 +0100

    8-load_from_json_file.py

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""load_from_json_file module"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    load_from_json_file python func
+    args :
+    my_obj
+    filename
+    """
+    with open(filename, 'w') as jsonf:
+        json.load(jsonf)
