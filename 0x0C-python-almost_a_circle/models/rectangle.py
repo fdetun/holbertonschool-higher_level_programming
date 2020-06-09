23b6c7eb911adc0985a148a6abaac76e46ffc71d
fdetun <fouedads@gmail.com>
2020-06-09 15:00:40 +0100

    rect

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,15 +6,13 @@ from models.base import Base
 class Rectangle(Base):
     """Rectangle Class"""
 
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """init func
         args:
         id (int)
         """
-        
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
         super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
