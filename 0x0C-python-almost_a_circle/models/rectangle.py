623d5c0b3a1bc936fc3f16416fce9f9ce9c556e9
fdetun <fouedads@gmail.com>
2020-06-09 15:05:12 +0100

    rect

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,7 +11,7 @@ class Rectangle(Base):
         args:
         id (int)
         """
-        super().__init__(id)
+        Base.__init__(self, id)
         self.__width = width
         self.__height = height
         self.__x = x
