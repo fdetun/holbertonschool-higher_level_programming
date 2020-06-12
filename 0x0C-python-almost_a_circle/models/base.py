c1fa5d83580f583b3c2ea97e40a73dd4a2d7a5da
fdetun <fouedads@gmail.com>
2020-06-13 00:35:48 +0100

    fdee

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,7 +23,7 @@ class Base:
         """
         function return json string
         """
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
