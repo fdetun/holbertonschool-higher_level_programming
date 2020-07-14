e10ce82f89558840bcf676ef9be382a78a0f487b
fdetun <fouedads@gmail.com>
2020-07-14 23:55:45 +0100

    14-my_genres.sql

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,4 @@
+-- join
+
+SELECT tv_genres.name FROM  tv_show_genres JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name;
