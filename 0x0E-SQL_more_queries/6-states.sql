83a9d16e5bb01b53a64d24b5024c3b5c11401f59
fdetun <fouedads@gmail.com>
2020-07-14 21:10:47 +0100

    6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- create database
+-- create table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states
+(id INT DEFAULT 1 UNIQUE AUTO_INCREMENT,
+name VARCHAR(256) NOt NULL,
+PRIMARY KEY (id)
+);
