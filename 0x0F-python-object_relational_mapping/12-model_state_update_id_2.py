7716c9896f8c4198b8424c7df21e14798ded10af
fdetun <fouedads@gmail.com>
2020-08-13 14:39:32 +0100

    12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""fetchall"""
+
+
+from sqlalchemy import create_engine
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    request = "mysql+mysqldb://{}:{}@localhost:3306/{}".\
+        format(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(request, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    session.query(State)\
+       .filter(State.id == 2)\
+       .update({State.name: "New Mexico"})
+    session.commit()
+    session.close()
