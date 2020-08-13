c10e2911fe7297cca353e112a898541b175ffc91
fdetun <fouedads@gmail.com>
2020-08-13 13:32:31 +0100

    10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""fetchall"""
+
+
+from sqlalchemy import create_engine
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    request = "mysql+mysqldb://{}:{}@localhost:3306/{}".\
+        format(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(request, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    f = session.query(State).filter(State.name==sys.argv[4]).first()
+    if f:
+        print(f.id)
+    else:
+        print("Not found")
+    session.close()
