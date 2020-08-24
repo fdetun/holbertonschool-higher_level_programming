6ea3f5c7d6fc023aabc5ec60874c70becc4055a3
fdetun <fouedads@gmail.com>
2020-08-24 23:32:38 +0100

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,12 +4,15 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    uname = sys.argv[1]
+    uname = sys.argv[2]
     api = "https://api.github.com/repos/"
-    url = api + uname + "/" + sys.argv[2] + "/commits"
+    url = api + uname + "/" + sys.argv[1] + "/commits"
     login = requests.get(url)
     f = login.json()
     for i in range(10):
-        a = f[i]["sha"]
-        b = f[i]["commit"]["author"]["name"]
-        print("{}: {}".format(a, b))
+        try:
+            a = f[i]["sha"]
+            b = f[i]["commit"]["author"]["name"]
+            print("{}: {}".format(a, b))
+        except IndexError:
+            pass
