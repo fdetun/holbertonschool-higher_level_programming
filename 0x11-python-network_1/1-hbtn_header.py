5121776dc28d80014c5c99a64d240386eef01cff
fdetun <fouedads@gmail.com>
2020-08-24 13:13:07 +0100

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """task 1"""
 import urllib.request as request
-
+import sys
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as s:
+    with request.urlopen(sys.argv[1]) as s:
         r = s.info()
         print(r['X-Request-Id'])
