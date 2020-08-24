4589b41d3838f10ecfe6e18a755b62f890541cc8
fdetun <fouedads@gmail.com>
2020-08-24 23:25:53 +0100

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,10 +5,9 @@ import sys
 
 if __name__ == "__main__":
     uname = sys.argv[1]
-    token = "2f0ff6a39beb16e93baef22b146fb6dd10e0903d"
     api = "https://api.github.com/repos/"
     url = api + uname + "/" + sys.argv[2] + "/commits"
-    login = requests.get(url, auth=(uname, token))
+    login = requests.get(url)
     f = login.json()
     for i in range(10):
         a = f[i]["sha"]
