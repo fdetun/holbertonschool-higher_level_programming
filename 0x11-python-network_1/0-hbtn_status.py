99a2999e2531814001c22efaf1c19d5247f91d89
fdetun <fouedads@gmail.com>
2020-08-24 12:06:40 +0100

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""task 0""""
+
+
+import urllib.request
+resource = urllib.request.urlopen("https://intranet.hbtn.io/status")
+html = resource.read()
+print("Body response:")
+print("\t- type: {}".format(html.__class__))
+print("\t- content: {}".format(html.__class__))
+print("\t- utf8 content: {}".format(html.decode("utf-8")))
