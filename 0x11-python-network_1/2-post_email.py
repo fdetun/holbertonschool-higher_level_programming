dbf14df7942038f3326ea78160cfeac3a63379dd
fdetun <fouedads@gmail.com>
2020-08-24 14:13:04 +0100

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,6 +4,7 @@ import urllib.request as request
 import urllib.parse as parse
 import sys
 
+
 if __name__ == "__main__":
     data = parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
     with request.urlopen(sys.argv[2], data) as s:
