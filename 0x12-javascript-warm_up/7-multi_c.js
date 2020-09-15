b48ae4053dcd1b60f009dbfbef718d3277e6f05d
fdetun <fouedads@gmail.com>
2020-09-15 17:11:03 +0100

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let i = 0;
+if (process.argv.length === 2) {
+  console.log('Missing number of occurrences');
+} else {
+  while (i < process.argv[2]) {
+    console.log('C is fun');
+    i++;
+  }
+}
