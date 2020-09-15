03bc75a124368bbb137e9aa9d4882415319a6213
fdetun <fouedads@gmail.com>
2020-09-15 15:56:53 +0100

    fix requir

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-if (process.argv.length == 3) { console.log('Argument found'); } else if (process.argv.length > 3) { console.log('Arguments found'); } else {
+if (process.argv.length === 3) { console.log('Argument found'); } else if (process.argv.length > 3) { console.log('Arguments found'); } else {
   console.log('No argument');
 }
