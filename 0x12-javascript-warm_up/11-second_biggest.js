87af2332bf1d7149a9edbd03dda2292ae7059695
fdetun <fouedads@gmail.com>
2020-09-16 12:25:22 +0100

    11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+let i = 2;
+const arr = [];
+if (process.argv.length === 3) {
+  console.log(0);
+} else {
+  while (i < process.argv.length) {
+    arr[i - 2] = process.argv[i];
+    i++;
+  }
+  arr.sort(function (a, b) { return a - b; });
+  console.log(arr[process.argv.length - 4]);
+}
