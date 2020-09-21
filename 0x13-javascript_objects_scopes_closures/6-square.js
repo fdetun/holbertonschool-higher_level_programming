17c96797c375c3e7fc61813597dca98c69d0842e
fdetun <fouedads@gmail.com>
2020-09-21 20:52:30 +0100

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const a = require('./5-square');
+
+module.exports = class Square extends a {
+  constructor (f) {
+    super(f);
+    this.fde = f;
+  }
+
+  charPrint (a) {
+    if (a == null) {
+      this.print();
+    } else {
+      let i = 0;
+      while (i < this.fde) {
+        console.log(a.repeat(this.fde));
+        i++;
+      }
+    }
+  }
+};
