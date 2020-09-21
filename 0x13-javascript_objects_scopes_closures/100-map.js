b315a2ba8750617088f614631e6896286a78ec74
fdetun <fouedads@gmail.com>
2020-09-21 23:29:59 +0100

    2222

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const i = require('./100-data').list;
-let j = []
-j = i.map(x => i.indexOf(x) * x)
+const j = [];
+i.map(x => j.push(i.indexOf(x) * x));
 console.log(i);
 console.log(j);
