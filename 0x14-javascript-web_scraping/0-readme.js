4e800317de68049acbfeab4b7b828e489e2322e7
fdetun <fouedads@gmail.com>
2020-09-22 16:37:52 +0100

    00

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 const fde = require('fs');
 
 fde.readFile(process.argv[2], 'utf8', function (error, datas) {
-  if (datas) {
-    console.log(datas);
-  } else {
+  if (error) {
     console.log(error);
+  } else {
+    console.log(datas);
   }
 });
