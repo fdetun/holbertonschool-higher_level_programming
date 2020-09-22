c0dac91c8baa5ccbde1533493059150704aded6d
fdetun <fouedads@gmail.com>
2020-09-22 19:27:51 +0100

    2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+
+const options = {
+  url: process.argv[2],
+  method: 'GET'
+};
+
+request(options, (err, reponse) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(reponse.statusCode);
+});
