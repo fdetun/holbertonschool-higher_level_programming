ec36a7fbeba0944457c9de0b06d002f505188f11
fdetun <fouedads@gmail.com>
2020-09-23 16:42:38 +0100

    100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/?format=json',
+  method: 'GET'
+};
+
+request(options, (err, reponse, body) => {
+  if (err) {
+    return console.log(err);
+  }
+  for (const cn of JSON.parse(body).characters) {
+    var nl = {
+      url: cn + '?format=json',
+      method: 'GET'
+    };
+    request(nl, (err, reponse, ok) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(JSON.parse(ok).name);
+    });
+  }
+});
